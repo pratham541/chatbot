dd47d192dc9fd6bcafee139bc70c6f86139c8f2f
pratham541 <prathamsurya076@gmail.com>
2025-06-30 09:28:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Simple Chatbot")
+
+# Ask the user to enter their name
+name = st.text_input("Enter your name:")
+
+# Display a greeting message
+if name:
+    st.write(f"Hello {name}!")
